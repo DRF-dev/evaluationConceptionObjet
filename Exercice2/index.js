5b599062721ea6769e8506933adcc3ab0d68603a
drf <dany.francisco@outlook.com>
2020-09-27 16:01:37 +0200

    feat(exercice2): creation of the static DrawBar

--- Exercice2/index.js
+++ Exercice2/index.js
@@ -9,3 +9,20 @@ var DrawBar = function DrawBar(sum, nbr) {
   this.sum = sum;
   this.nbr = nbr;
 }
+
+DrawBar.prototype.run = function() {
+  var drawBar = document.createElement('div');
+  var fullBar = document.createElement('div');
+
+  drawBar.style.width = "90vw";
+  drawBar.style.height = "5vh";
+  drawBar.style.margin = "0 auto";
+  drawBar.style.border = "1px solid black";
+
+  fullBar.style.width = "40%";
+  fullBar.style.height = "100%";
+  fullBar.style.backgroundColor = "green";
+
+  drawBar.appendChild(fullBar);
+  document.body.appendChild(drawBar);
+}
