af6bb060fbd9a164ca0787407b81cb1be8ed24c2
drf <dany.francisco@outlook.com>
2020-09-29 23:14:57 +0200

    feat(exercice3): add the method who will create our first morpion

---
+++ Exercice3/index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var MyMorpionXO = function MyMorpionXO() {
+
+}
+
+MyMorpionXO.prototype.createNewTable = function() {
+  var table = document.createElement('table');
+  table.style.borderCollapse = "collapse";
+  table.style.margin = "30% auto";
+
+  for (var i = 0; i < 3; i++) {
+    var line = document.createElement('tr');
+    for (var j = 0; j < 3; j++) {
+      var square = document.createElement('th');
+      square.style.border = "0.5px solid black";
+      square.style.width = "80px";
+      square.style.height = "80px";
+      if (i === 0) {
+        square.style.borderTop = "hidden";
+      }
+      if (i === 2) {
+        square.style.borderBottom = "hidden";
+      }
+      if (j === 0) {
+        square.style.borderLeft = "hidden";
+      }
+      if (j === 2) {
+        square.style.borderRight = "hidden";
+      }
+      line.appendChild(square);
+    }
+    table.appendChild(line);
+  }
+  document.body.appendChild(table);
+}
+
+var morpion = new MyMorpionXO();
+morpion.createNewTable();
