e1a59de763b27f67346620b67923c7aa996ccb25
drf <dany.francisco@outlook.com>
2020-09-30 21:57:02 +0200

    feat(exercice3): add the possibility to write a cross or a circle

--- Exercice3/index.js
+++ Exercice3/index.js
@@ -13,7 +13,7 @@ var MyMorpionXO = function MyMorpionXO() {
 MyMorpionXO.prototype.createNewTable = function() {
   var table = document.createElement('table');
   table.style.borderCollapse = "collapse";
-  table.style.margin = "30% auto";
+  table.style.margin = "auto";
 
   for (var i = 0; i < 3; i++) {
     var line = document.createElement('tr');
@@ -41,5 +41,63 @@ MyMorpionXO.prototype.createNewTable = function() {
   document.body.appendChild(table);
 }
 
+MyMorpionXO.prototype.turn = function(e) {
+  //If the square is not a "th" or is occupated, we canceld 
+  if (e.target.tagName !== "TH" || e.target.hasChildNodes()) {
+    return;
+  }
+  e.target.appendChild(this.cross());
+}
+
+/**
+ * circle : add a circle
+ */
+MyMorpionXO.prototype.circle = function() {
+  var circle = document.createElement('div');
+  circle.style.width = "75%";
+  circle.style.height = "75%";
+  circle.style.margin = "auto";
+  circle.style.border = "2px solid black";
+  circle.style.borderRadius = "90px";
+  return circle;
+}
+
+/**
+ * cross : add a cross
+ */
+MyMorpionXO.prototype.cross = function() {
+  var cross = document.createElement('div');
+
+  for (var i = 0; i < 2; i++) {
+    var bar = document.createElement('div');
+    bar.style.width = "100%";
+    bar.style.margin = "auto";
+    bar.style.border = "2px solid black";
+    if (i === 0) {
+      bar.style.transform = "rotate(45deg)";
+    }
+    if (i === 1) {
+      bar.style.transform = "rotate(-45deg)";
+    }
+    cross.appendChild(bar);
+  }
+  return cross;
+}
+
+/**
+ * run : initialize a new game
+ */
+MyMorpionXO.prototype.run = function() {
+  if (!document.querySelector('table')) {
+    this.createNewTable();
+  }
+  var squares = document.querySelectorAll('th');
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].addEventListener('click', function(e) {
+      this.turn(e);
+    }.bind(this));
+  }
+}
+
 var morpion = new MyMorpionXO();
-morpion.createNewTable();
+morpion.run();
