ce3e2532faa68e8642de90f2f456daaa8528d3c5
drf <dany.francisco@outlook.com>
2020-10-03 14:12:49 +0200

    feat(exercice3): add a condition of victory and if this condition is completed end the game

--- Exercice3/index.js
+++ Exercice3/index.js
@@ -5,6 +5,7 @@
  */
 var MyMorpionXO = function MyMorpionXO() {
   this.round = 0;
+  this.end = false;
 }
 
 /**
@@ -46,15 +47,43 @@ MyMorpionXO.prototype.createNewTable = function() {
  * @param {e} e : This is the event who activate this method 
  */
 MyMorpionXO.prototype.turn = function(e) {
-  this.round++;
   //If the square is not a "th" or is occupated, we canceld 
-  if (e.target.tagName !== "TH" || e.target.hasChildNodes()) {
+  if (e.target.tagName !== "TH" || e.target.hasChildNodes() || this.end) {
     return;
   }
+  this.round++;
   if (this.round % 2 === 0) {
     return e.target.appendChild(this.cross());
   }
   return e.target.appendChild(this.circle());
+}
+/**
+ * endGame : verify if one of the player have win or not
+ */
+MyMorpionXO.prototype.endGame = function() {
+  if (this.end) {
+    return;
+  }
+  var squares = document.querySelectorAll('th');
+  var firstLine = squares[0].childNodes[0]?.className * squares[1].childNodes[0]?.className * squares[2].childNodes[0]?.className;
+  var secondLine = squares[3].childNodes[0]?.className * squares[4].childNodes[0]?.className * squares[5].childNodes[0]?.className;
+  var thirdLine = squares[6].childNodes[0]?.className * squares[7].childNodes[0]?.className * squares[8].childNodes[0]?.className;
+  var firstColumn = squares[0].childNodes[0]?.className * squares[3].childNodes[0]?.className * squares[6].childNodes[0]?.className;
+  var secondColumn = squares[1].childNodes[0]?.className * squares[4].childNodes[0]?.className * squares[7].childNodes[0]?.className;
+  var thirdColumn = squares[2].childNodes[0]?.className * squares[5].childNodes[0]?.className * squares[8].childNodes[0]?.className
+  var firstDiagonal = squares[0].childNodes[0]?.className * squares[4].childNodes[0]?.className * squares[8].childNodes[0]?.className;
+  var secondDiagonal = squares[2].childNodes[0]?.className * squares[4].childNodes[0]?.className * squares[6].childNodes[0]?.className;
+
+  if (firstLine === 1 || secondLine === 1 || thirdLine === 1 ||
+    firstColumn === 1 || secondColumn === 1 || thirdColumn === 1 ||
+    firstDiagonal === 1 || secondDiagonal === 1) {
+    this.messagesVictory(1);
+  }
+  if (firstLine === 8 || secondLine === 8 || thirdLine === 8 ||
+    firstColumn === 8 || secondColumn === 8 || thirdColumn === 8 ||
+    firstDiagonal === 8 || secondDiagonal === 8) {
+    this.messagesVictory(2);
+  }
 }
 
 /**
@@ -62,6 +91,7 @@ MyMorpionXO.prototype.turn = function(e) {
  */
 MyMorpionXO.prototype.circle = function() {
   var circle = document.createElement('div');
+  circle.className = 1;
   circle.style.width = "75%";
   circle.style.height = "75%";
   circle.style.margin = "auto";
@@ -75,6 +105,7 @@ MyMorpionXO.prototype.circle = function() {
  */
 MyMorpionXO.prototype.cross = function() {
   var cross = document.createElement('div');
+  cross.className = 2;
 
   for (var i = 0; i < 2; i++) {
     var bar = document.createElement('div');
@@ -93,6 +124,17 @@ MyMorpionXO.prototype.cross = function() {
 }
 
 /**
+ * messageVictory : show the message of vitory
+ * @param {Number} player : the player 1 or the player 2
+ */
+MyMorpionXO.prototype.messagesVictory = function(player) {
+  var messagesWin = document.createElement('span');
+  messagesWin.textContent = "Felicitation joueur " + player + " , vous avez gagné !";
+  this.end = true;
+  return document.body.appendChild(messagesWin);
+}
+
+/**
  * run : initialize a new game
  */
 MyMorpionXO.prototype.run = function() {
@@ -103,6 +145,7 @@ MyMorpionXO.prototype.run = function() {
   squares.forEach(function(square) {
     square.addEventListener('click', function(e) {
       this.turn(e);
+      this.endGame();
     }.bind(this));
   }.bind(this))
 }
