a82737c4a3d9092b16a037dd716590d0c0a2f59f
drf <dany.francisco@outlook.com>
2020-10-07 21:49:46 +0200

    feat(exercice5): add the button and rewrite the function that generate html element

--- Exercice5/index.js
+++ Exercice5/index.js
@@ -9,14 +9,18 @@ var MyForm = function MyForm() {
 
 /**
  * generateInput : return an input with a type and a placeholder
- * @param {String} type : the type of the input
- * @param {String} placeholder : the placeholder of the input
+ * @param {String} element A html's element
+ * @param {String} type the type of the input
+ * @param {String} placeholder the placeholder of the input
  */
-MyForm.prototype.generateInput = function(type, placeholder) {
-  var input = document.createElement('input');
-  input.type = type;
-  input.placeholder = placeholder;
-  return input;
+MyForm.prototype.generateElement = function(element, type, placeholder = undefined, textContent = undefined) {
+  var container = document.createElement('div');
+  var element = document.createElement(element);
+  element.type = type;
+  placeholder? element.placeholder = placeholder : null;
+  textContent? element.textContent = textContent : null;
+  container.appendChild(element);
+  return container;
 }
 
 /**
@@ -24,14 +28,15 @@ MyForm.prototype.generateInput = function(type, placeholder) {
  */
 MyForm.prototype.addForm = function() {
   var form = document.createElement('form');
-  var inputs = {
-    'lastName': this.generateInput('text', 'Last name'),
-    'firstName': this.generateInput('text', 'First name'),
-    'email': this.generateInput('email', 'Email'),
-    'password': this.generateInput('password', 'password')
+  var html = {
+    'lastName': this.generateElement('input', 'text', 'Last name'),
+    'firstName': this.generateElement('input', 'text', 'First name'),
+    'email': this.generateElement('input', 'email', 'Email'),
+    'password': this.generateElement('input', 'password', 'password'),
+    'button': this.generateElement('button', 'submit', null, 'Envoyer')
   }
-  for (const key in inputs) {
-    form.appendChild(inputs[key]);
+  for (const key in html) {
+    form.appendChild(html[key]);
   }
   document.body.appendChild(form);
 }
