80c757668ca5bf2a8387ada331bd5ed58b621b15
drf <dany.francisco@outlook.com>
2020-10-11 11:22:14 +0200

    feat(exercice7): show all the grades and the average grade of a student

--- Exercice7/index.js
+++ Exercice7/index.js
@@ -1,1 +1,41 @@
 'use strict';
+
+/**
+ * ComputeNotes: Calculate the average of a student's grades
+ * @param {number[]} grades all the grades of a student
+ */
+var ComputeNotes = function ComputeNotes(grades) {
+  this.grades = grades;
+}
+
+/**
+ * Calculate the average grade of the student
+ */
+ComputeNotes.prototype.averageGrade = function() {
+  var sumNotes = 0;
+  this.grades.forEach(function(grade) {
+    sumNotes += grade
+  })
+
+  return (sumNotes/this.grades.length).toFixed(2)
+}
+
+/**
+ * run our app
+ */
+ComputeNotes.prototype.run = function() {
+  var textHtml = {
+    'listGrades': document.createElement('p'),
+    'averageGrade': document.createElement('span')
+  }
+
+  textHtml['listGrades'].textContent = 'Cet étudiant à pour notes : ' + this.grades;
+  textHtml['averageGrade'].textContent = 'Ce qui lui fait ' + this.averageGrade() + ' de moyenne';
+
+  for (const key in textHtml) {
+    document.body.appendChild(textHtml[key])
+  }
+}
+
+var student = new ComputeNotes([12, 12, 12, 13, 19, 18, 18, 19, 19, 6, 3, 3, 3, 17, 18]);
+student.run();
