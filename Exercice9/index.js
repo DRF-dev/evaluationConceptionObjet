026b2cfa5c7d7a533a8c5f90bdc0864c32e6e2c5
drf <dany.francisco@outlook.com>
2020-10-11 12:33:03 +0200

    feat(exercice9): check if the word is a palindrome

--- Exercice9/index.js
+++ Exercice9/index.js
@@ -1,1 +1,26 @@
 'use strict';
+
+/**
+ * check if the word or the sentence are a palindrom
+ * @param {string} str a word or a sentence (remove the whitespace at the began and the end)
+ */
+var CheckPalindrome = function CheckPalindrome(str) {
+  this.str = str.trim();
+}
+
+/**
+ * Check if we have a palindrome
+ * @param {boolean} isUniqueWord true if it is a unique word or false if it is a sentence
+ */
+CheckPalindrome.prototype.isPalindrome = function(isUniqueWord) {
+  var lowerStr = this.str.toLowerCase();
+  var arrayStr = isUniqueWord? lowerStr.split('') : lowerStr.split(' ');
+  var backwards = '';
+  for (var i = arrayStr.length - 1; i >= 0; i--) {
+    backwards += arrayStr[i]
+  }
+  return backwards === lowerStr;
+}
+
+var newPalindrome = new CheckPalindrome('kayaks');
+console.log(newPalindrome.isPalindrome(true))
